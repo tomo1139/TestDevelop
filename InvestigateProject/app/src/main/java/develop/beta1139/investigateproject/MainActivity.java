b7154a607fa6626ec74ba50f6edbe17c3a8011c0
tomo1139 <tomo1139@gmail.com>
2016-09-18 12:14:28 +0900

    サービスの定時実行処理の実装

--- InvestigateProject/app/src/main/java/develop/beta1139/investigateproject/MainActivity.java
+++ InvestigateProject/app/src/main/java/develop/beta1139/investigateproject/MainActivity.java
@@ -1,7 +1,11 @@
 package develop.beta1139.investigateproject;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Test
+        startUpdateNewsService();
+    }
+
+    private void startUpdateNewsService() {
+        Log.e("dbg", Thread.currentThread().getStackTrace()[2].getMethodName() + " called!!");
+
+        Intent intent = new Intent(this, UpdateNewsService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(
+                this, -1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        long targetMillis = UpdateNewsService.getTargetMillis();
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, targetMillis, AlarmManager.INTERVAL_DAY,
+                pendingIntent);
+
+        //alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, targetMillis, 10 * 1000, pendingIntent);
     }
 }
+
